9d13bf14e7100d105aebaeb0e41185ce309c4f5b
patw <patw.hi@gmail.com>
2017-04-20 00:21:30 +0800

    Header component

---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Header = (props) => {
+  return (
+    <View style={styles.wrapper}>
+      <Text style={styles.title}>{props.title}</Text>
+    </View>
+  );
+};
+
+Header.displayName = 'Header';
+
+const styles = {
+  wrapper: {
+    backgroundColor: '#f8f8f8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative',
+  },
+  title: {
+    fontSize: 20,
+  },
+};
+
+export default Header;
