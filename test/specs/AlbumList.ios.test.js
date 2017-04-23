b99a17be268dcf84a4f60926fd51e24d6ecac54d
patw <patw.hi@gmail.com>
2017-04-24 00:39:50 +0800

    Test swipe gesture

--- test/specs/AlbumList.ios.test.js
+++ test/specs/AlbumList.ios.test.js
@@ -1,3 +1,5 @@
+import { TouchAction } from 'wd';
+
 const { getDriver, runApp } = appiumUtils;
 
 describe('AlbumList', () => {
@@ -11,24 +13,37 @@ describe('AlbumList', () => {
     driver.setImplicitWaitTimeout(20000);
   });
 
-  it('should get last item`s title 1989', () => {
+  it('should get last item`s title 1989', async () => {
+    const el1 = await driver.elementsByAccessibilityId('AlbumDetail').at(0);
+    const el2 = await driver.elementsByAccessibilityId('AlbumDetail').at(1);
+    const action = new TouchAction(driver);
+    const scrollDown = action.press({ y: 70 }) // press near the bottom of the list
+      .moveTo({ y: 300 }) // drag finger up
+      .release(); // release finger
 
-    return driver
-      .waitForElementsByAccessibilityId('AlbumDetail').last()
-      .source((err, code) => {
-        console.log(code);
-      })
-      .then((el) => {
-        console.log(el);
+    await driver.performTouchAction(scrollDown);
 
-        const scrollOpts = {
-          element: el.value,
-          toVisible: true,
-        };
+    // return driver
+    //   .waitForElementByAccessibilityId('AlbumDetail')
+    //   .sleep(2000)
+    //   .performTouchAction(scrollUp)
 
-        return driver.execute("mobile: scroll", [scrollOpts]);
-      })
-      .sleep(10000);
+    // return driver
+    //   .waitForElementsByAccessibilityId('AlbumDetail').last()
+    //   .source((err, code) => {
+    //     console.log(code);
+    //   })
+    //   .then((el) => {
+    //     console.log(el);
+
+    //     const scrollOpts = {
+    //       element: el.value,
+    //       toVisible: true,
+    //     };
+
+    //     return driver.execute("mobile: scroll", [scrollOpts]);
+    //   })
+    //   .sleep(10000);
       // .elementByXPath('//XCUIElementTypeApplication[1]/XCUIElementTypeWindow[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[2]/XCUIElementTypeScrollView[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[3]/XCUIElementTypeOther[1]')
       // .source((err, code) => {
       //   console.log(code);
