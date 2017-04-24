0d03ff4bfd2bb9ff85419700c3ec269e5e313318
Patrick <patw.hi@gmail.com>
2017-04-24 13:47:43 +0800

    Removed unused part

--- test/utils/compile.js
+++ test/utils/compile.js
@@ -5,7 +5,6 @@ const cachingTransform = require('caching-transform');
 const ignore = /node_modules\/(?!(react-tools|react-native-.*|texts)\/)/;
 
 require('babel-polyfill');
-// require('react-native/packager/react-packager/src/Resolver/polyfills/babelHelpers.js');
 require('asset-require-hook')({
   extensions: ['png', 'jpg'],
   name: '[name].[ext]?[sha512:hash:base64:7]'
@@ -23,20 +22,17 @@ require.extensions['.js'] = function (module, filename) {
   if (filename.indexOf('node_modules/react-native/Libraries/react-native/react-native.js') >= 0) {
     filename = path.resolve('./test/utils/react-native.js');
   }
+
   const src = fs.readFileSync(filename, 'utf8');
+
   if (ignore && ignore.test(filename)) {
     return module._compile(src, filename);
   }
+
   const result = cachedtransform(src, {
     filename,
     sourceFileName: filename,
     presets: ['react-native'],
-    plugins: [
-      ['babel-plugin-module-alias', [
-        {src: './app/common', expose: 'common'},
-        {src: './app/', expose: 'app'}
-      ]]
-    ]
   });
 
   module._compile(result, filename);
