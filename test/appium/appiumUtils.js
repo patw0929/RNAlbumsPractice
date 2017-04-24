69be6b07cb510ed6fc2c90fdaa1389098723e646
Patrick <patw.hi@gmail.com>
2017-04-24 14:05:07 +0800

    Removed unused part

--- test/appium/appiumUtils.js
+++ test/appium/appiumUtils.js
@@ -1,35 +1,11 @@
 import * as webDriver from './webDriver';
 import * as actionServer from './actionServer';
 
-const EXECUTE_ACTION_ELEMENT_ID = 'Execute';
-const START_PAGE_ELEMENT_ID = 'Login';
-
 export function runApp() {
   before(() => {
     return webDriver.createDriver()
       .then(() => actionServer.startServer());
   });
 }
-
-export function callApp(opts) {
-  if (opts.actions) {
-    opts.actions.forEach(actionServer.action);
-  }
-
-  if (opts.redirects) {
-    opts.redirects.forEach(actionServer.redirect);
-  }
-
-  return webDriver.getDriver()
-    .elementByAccessibilityId(EXECUTE_ACTION_ELEMENT_ID)
-    .click()
-}
-
-export function redirect(name, params) {
-  return callApp({ redirects: [{ name, params }] });
-}
-
-const logout = () => callApp(actions.logout)
-  .waitForElementByAccessibilityId(START_PAGE_ELEMENT_ID);
 
 export const getDriver = webDriver.getDriver;
