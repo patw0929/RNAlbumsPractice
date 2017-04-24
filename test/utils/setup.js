7fb1b0691fe251c3db283819c068e971455fd538
Patrick <patw.hi@gmail.com>
2017-04-24 10:57:39 +0800

    Support should syntax

--- test/utils/setup.js
+++ test/utils/setup.js
@@ -4,9 +4,11 @@ import chaiEnzyme from 'chai-enzyme';
 import * as appiumUtils from '../appium/appiumUtils';
 import * as testConfig from '../appium/testConfig';
 
+chai.should();
 chai.use(sinonChai);
 chai.use(chaiEnzyme());
 
+global.should = chai.should;
 global.expect = chai.expect;
 global.appiumUtils = appiumUtils;
 global.testConfig = testConfig;
